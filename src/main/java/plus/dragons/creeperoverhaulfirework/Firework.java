fbb2d1c10843581934000f1a3f189f5953231a6b
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-09-22 22:27:26 +0800

    setup

---
+++ src/main/java/plus/dragons/creeperoverhaulfirework/Firework.java
@@ -0,0 +1,9 @@
+package plus.dragons.creeperoverhaulfirework;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Firework implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
